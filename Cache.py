78809deb23beb4992b88cbbfa668fc73129008ad
Prem Kumar <prembammidi25@msitprogram.net>
2020-04-14 00:22:46 +0530

    Code Commit

--- Cache.py
+++ Cache.py
@@ -1,27 +1,52 @@
 from datetime import datetime
 
+class LRUitem(object):
+    def __init__(self, key, item) :
+        self.key = key
+        self.item = item
+        self.timestamp = datetime.now()
+
 class LRU(object) :
-    def __init__(self, capacity, time) :
-        self.capacity = capacity
-        self.time = time
-        self.hash_table = {}
+    def __init__(self, length, delta=None):
+        self.length = length
+        self.delta = delta
+        self.hash = {}
         self.item_list = []
 
     def put(self, item) :
-        pass
-    
+        # pass
+        if item.key in self.hash:
+            item_index = self.item_list.index(item)
+            self.item_list[:] = self.item_list[:item_index] + self.item_list[item_index + 1:]
+            self.item_list.insert(0, item)
+        else :
+            if len(self.item_list) > self.length:
+                self.delete(self.item_list[-1])
+            self.hash[item.key] = item
+            self.item_list.insert(0, item)
+
 
     def delete(self, item):
-        pass
+        # pass
+        del self.hash[item.key]
+        del self.item_list[self.item_list.index(item)]
 
     def check(self) :
-        pass
+        # pass
+        def old_items():
+            recent = datetime.now()
+            for item in self.item_list:
+                time = recent - item.timestamp
+                if time.seconds > self.delta:
+                    yield item
+        map(lambda x: self.delete(x), old_items())
 
     def get(self, item) :
-        pass
+        # pass
+        if item.key in self.hash:
+            item_index = self.item_list.index(item)
+            yield self.item_list[item_index]
 
-    def print_cache(object) :
-        pass
 
 # def main():
 #     LRU
--- LRUtest.py
+++ LRUtest.py
@@ -1,16 +1,46 @@
 import unittest
+from Cache import *
+from time import sleep
 
 class TestMethods(unittest.TestCase) :
-    def setUp(self):
-        return super().setUp()
+    # def setUp(self):
+    #     return super().setUp()
+    # 
+    
+    def print_cache(cache) :
+        # pass
+        for i, item in enumerate(cache.item_list) :
+            print (f"index: {i} " + f"key: {item.key} " + f"item: {item.item} " + f"time: {item.timestamp} ")
 
+    print ("Initial cache items.")
     one = LRUitem(1, 'one')
     two = LRUitem(2, 'two')
     three = LRUitem(3, 'three')
-    cache = LruCache(length = 3, capacity = 5)
+    cache = LRU(length = 3, delta = 5)
+    cache.put(one)
+    cache.put(two)
+    cache.put(three)
+    print_cache(cache)
+    print ("#" * 20)
 
-    def cache(self) :
-        self.assertEqual(cache.get(one), 'one')
+    # print ("Insert a existing item: {0}.".format(one.key))
+    # cache.put(one)
+    # print_cache(cache)
+    # print ("#" * 20)
+
+    # print ("Insert another existing item: {0}.".format(two.key))
+    # cache.put(two)
+    # print_cache(cache)
+    # print ("#" * 20)
+
+    # print ("Validate items after a period of time")
+    # sleep(6)
+    # cache.check()
+    # print_cache(cache)
+    # print ("#" * 20)
+
+    # def cache(self) :
+    #     self.assertEqual(cache.get(one), 'one')
 
 
 if __name__ == '__main__' :

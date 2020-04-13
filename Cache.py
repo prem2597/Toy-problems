7d8482b314b3eea5550d09c7b98a2b86af1dafe6
Prem Kumar <prembammidi25@msitprogram.net>
2020-04-13 20:37:56 +0530

    add constructor for LRU

---
+++ Cache.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+class LRU(object) :
+    def __init__(self, capacity, time) :
+        self.capacity = capacity
+        self.time = time
+        self.hash_table = {}
+        self.item_list = []
+
+def main():
+    LRU
+
+if __name__ == "__main__":
+    main()
